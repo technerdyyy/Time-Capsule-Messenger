10b85a213279e1f97589d1a3364a5618864544f8
Devyani Kumari <devyanik457@gmail.com>
2025-02-26 18:26:33 +0530

    mongoDB connection pending

---
+++ backend/models/UserModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "provide name"]
+    },
+    email: {
+        type: String,
+        required: [true, "provide email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "provide password"]
+    },
+},{
+        timestamps: true
+})
+
+const UserModel = mongoose.model('User',userSchema)
+
+module.exports = UserModel
