57ae2dac696ca7496da3535c582e929681c788c2
Your Name <your-email@example.com>
2025-02-27 20:41:58 +0530

    redux added

---
+++ frontend/src/redux/userSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    _id:"",
+ name : "",
+ email : "",
+ profile_pic:"",
+ token:"",
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state,action)=>{
+        state.id = action.payload._id
+        state.name = action.payload.name
+        state.email = action.payload.email
+        state.profile_pic = action.payload.profile_pic
+    }
+    setToken : (state,action)=>{
+        state.token = action.payload
+    },
+    logout : (state,action)=>{
+        state.id = ""
+        state.name = ""
+        state.email = ""
+        state.profile_pic = ""
+
+    }
+   
+  },
+})
+
+
+export const { setUser, setToken , logout } = userSlice.actions
+
+export default userSlice.reducer
