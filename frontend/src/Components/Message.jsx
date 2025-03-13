57ef5cbf38ffc06502a23ca48c206b4e2838951e
Devyani Kumari <devyanik457@gmail.com>
2025-03-14 02:15:41 +0530

    cron nodemailer 1

--- frontend/src/Components/Message.jsx
+++ frontend/src/Components/Message.jsx
@@ -13,7 +13,7 @@ const Message = () => {
       const response = await axios.post(
         `${import.meta.env.VITE_BACKEND_URL}/api/schedule-message`,
         {
-          sender: "", // Replace with logged-in user's email dynamically
+          sender: "", 
           recipient,
           subject,
           body,
